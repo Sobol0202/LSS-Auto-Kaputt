44b97695bd15429cb8165fb6aa43f372b8c6bb68
Sobol0202 <67016865+Sobol0202@users.noreply.github.com>
2023-09-15 19:15:24 +0200

    Update LSS Auto kaputt.user.js
    // fixed Distance Problem

--- LSS Auto kaputt.user.js
+++ LSS Auto kaputt.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LSS Auto kaputt
 // @namespace    www.leitstellenspiel.de
-// @version      1.9
+// @version      1.10
 // @description  Simuliert den Ausfall von Fahrzeugen aus technischen Gründen
 // @author       MissSobol
 // @match        https://www.leitstellenspiel.de/
@@ -33,6 +33,43 @@
         setTimeout(() => {
             document.body.removeChild(popup);
         }, 5000);
+    }
+
+    // Funktion, um den Kilometerstand eines Fahrzeugs abzurufen
+    function getVehicleDistance(vehicleId, caption) {
+        fetch('https://www.leitstellenspiel.de/api/v1/vehicle_distances.json')
+            .then(response => response.json())
+            .then(distanceData => {
+                // Durchsuchen Sie die Liste der Kilometerstände nach der vehicle_id des ausgewählten Fahrzeugs
+                const vehicleDistance = distanceData.result.find(item => item.vehicle_id === vehicleId || item.id === vehicleId);
+                if (vehicleDistance && typeof vehicleDistance.distance_km !== 'undefined') {
+                    const distance = vehicleDistance.distance_km;
+                    console.log(`Fahrzeug "${caption}", ID: ${vehicleId}, Kilometerstand: ${distance} km`);
+
+                    // Berechnung der Wahrscheinlichkeit eines Totalschadens hier...
+                                // In den folgenden 4 Zeilen kann die Wahrscheinlichkeit eines Totalausfalls eingestellt werden. Es darf immer nur 1 Zeile aktiv sein. Zum aktivieren die beiden Linien am Anfang der Zeile entfernen. Zum deaktivieren diese wieder einfügen.
+                                //const probability = 101; //Jedes Fahrzeug ist Totalausfall
+                                //const probability = Math.exp(1.23072 + 6.98756e-05 * distance); //Fahrzeuge über 55kkm sind immer Totalschaden
+                                const probability = Math.cosh((6.86197e-05) * (distance - 1.09594)); //Fahrzeuge über 100kkm sind immer Totalschaden
+                                //const probability = coshyp((-3.45427e-05) * (distance - 32928)); //Fahrzeuge über 200kkm sind immer Totalschaden
+                    console.log(`Berechnete Wahrscheinlichkeit eines Totalschadens: ${probability}`);
+
+                    // Überprüfen, ob das Fahrzeug einen Totalschaden erleidet
+                    if (probability >= 100 || probability >= Math.random() * 100) {
+                        // Das Fahrzeug erleidet einen Totalschaden
+                        setVehicleOutOfService(vehicleId, caption, true);
+                    } else {
+                        // Das Fahrzeug muss für 10 Minuten in die Werkstatt
+                        setVehicleOutOfService(vehicleId, caption, false);
+                        localStorage.setItem('outOfServiceVehicle', JSON.stringify({ vehicleId: vehicleId, timestamp: new Date().getTime(), caption }));
+                    }
+                } else {
+                    console.error(`Kilometerstand für Fahrzeug "${caption}", ID: ${vehicleId} nicht gefunden oder undefiniert.`);
+                }
+            })
+            .catch(error => {
+                console.error(`Fehler beim Abrufen des Kilometerstands für Fahrzeug "${caption}", ID: ${vehicleId}:`, error);
+            });
     }
 
     // Funktion, um ein Fahrzeug außer Dienst zu setzen
@@ -67,7 +104,15 @@
         });
     }
 
-    // Funktion, um eine Nachricht an den Spieler zu senden
+    // Funktion zur Berechnung des Intervalls basierend auf der Fahrzeuganzahl
+    function calculateInterval(vehicleCount) {
+        const minTime = Math.pow(1.666649e-07 * vehicleCount, -0.307978);
+        const maxTime = minTime + (minTime * 0.5);
+        // Umrechnung von Millisekunden in Minuten
+        return getRandomInt(minTime, maxTime) / 0.001 * 60;
+    }
+
+    // Funktion zur Nachrichtenversendung an den Spieler
     function sendMessageToPlayer(messageBody) {
         // Authentifizierungstoken (CSRF-Token) abrufen
         const authToken = document.querySelector('meta[name="csrf-token"]')?.getAttribute('content');
@@ -125,9 +170,6 @@
             .then(data => {
                 const vehicleCount = data.length;
 
-                // Berechnen Sie das Intervall basierend auf der Fahrzeuganzahl
-                const interval = calculateInterval(vehicleCount);
-
                 // Prüfen, ob bereits ein Fahrzeug außer Dienst ist
                 const storedVehicle = localStorage.getItem('outOfServiceVehicle');
                 if (storedVehicle) {
@@ -146,27 +188,8 @@
                         const selectedVehicle = vehiclesWithFMS2[randomIndex];
                         const { id, caption } = selectedVehicle;
 
-                        // Kilometerstand des Fahrzeugs abrufen
-                        fetch(`https://www.leitstellenspiel.de/api/v1/vehicle_distances.json?id=${id}`)
-                            .then(response => response.json())
-                            .then(distanceData => {
-                                const distance = distanceData.distance_km;
-
-                                // In den folgenden 4 Zeilen kann die Wahrscheinlichkeit eines Totalausfalls eingestellt werden. Es darf immer nur 1 Zeile aktiv sein. Zum aktivieren die beiden Linien am Anfang der Zeile entfernen. Zum deaktivieren diese wieder einfügen.
-                                //const probability = 101; //Jedes Fahrzeug ist Totalausfall
-                                //const probability = Math.exp(1.23072 + 6.98756e-05 * distance); //Fahrzeuge über 55kkm sind immer Totalschaden
-                                const probability = Math.cosh((6.86197e-05) * (distance - 1.09594)); //Fahrzeuge über 100kkm sind immer Totalschaden
-                                //const probability = coshyp((-3.45427e-05) * (distance - 32928)); //Fahrzeuge über 200kkm sind immer Totalschaden
-
-                                if (probability >= 100 || probability >= Math.random() * 100) {
-                                    // Das Fahrzeug erleidet einen Totalschaden
-                                    setVehicleOutOfService(id, caption, true);
-                                } else {
-                                    // Das Fahrzeug muss für 10 Minuten in die Werkstatt
-                                    setVehicleOutOfService(id, caption, false);
-                                    localStorage.setItem('outOfServiceVehicle', JSON.stringify({ vehicleId: id, timestamp: new Date().getTime(), caption }));
-                                }
-                            });
+                        // Kilometerstand des ausgewählten Fahrzeugs abrufen und in der Konsole anzeigen
+                        getVehicleDistance(id, caption);
                     }
                 }
 
@@ -175,14 +198,7 @@
             });
     }
 
-    // Funktion zur Berechnung des Intervalls basierend auf der Fahrzeuganzahl
-    function calculateInterval(vehicleCount) {
-        const minTime = Math.pow(1.666649e-07 * vehicleCount, -0.307978);
-        const maxTime = minTime + (minTime * 0.5);
-        // Umrechnung von Millisekunden in Minuten
-        return getRandomInt(minTime, maxTime) / 0.001 * 60;
-    }
-
     // Skript starten
     simulateVehicleFailure();
 })();
+
